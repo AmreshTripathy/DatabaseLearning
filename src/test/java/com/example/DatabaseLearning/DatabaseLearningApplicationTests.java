603700af9d3b0e39c100fd587ac1de3c8f63136c
AmreshTripathy <amreshtripathy184@gmail.com>
2023-06-11 22:50:43 +0530

    Push 1

---
+++ src/test/java/com/example/DatabaseLearning/DatabaseLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DatabaseLearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
